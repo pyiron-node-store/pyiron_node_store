cfed0280d276bd53cadf30b0d6aa4eac32816b28
Niklas Siemer <70580458+niklassiemer@users.noreply.github.com>
2025-04-28 16:57:24 +0200

    Improved error handling

--- pyiron_node_store/__init__.py
+++ pyiron_node_store/__init__.py
@@ -1,18 +1,66 @@
 from importlib.metadata import entry_points
 import sys
+import types
+from ._version import get_versions as _get_versions
+from warnings import warn
 
-from ._version import get_versions
+# might be very interesting to get tests at some point:
+# https://stackoverflow.com/questions/40514205/how-to-dynamically-add-and-load-entry-points
 
-__version__ = get_versions()["version"]
+__version__ = _get_versions()["version"]
 
-module_name = 'pyiron_node_store'
-registered_nodes = {}
-for node in entry_points():
-    if node.group.startswith(module_name + '.'):
-        sub_module = node.group[len(module_name)+1:]
-        registered_nodes["pyiron_node_store." + sub_module + '.' + node.name] = node
 
-print(registered_nodes)
+del _get_versions
 
-for key, value in registered_nodes.items():
-    sys.modules[key] = value.load()
+_MODULE_NAME = "pyiron_node_store"
+_DEFAULT_SUB_MODULE_NAME = "nodes"
+
+nodes = types.ModuleType(_DEFAULT_SUB_MODULE_NAME)
+_SUB_MODULES = {_DEFAULT_SUB_MODULE_NAME: nodes}
+
+
+def _find_entry_points():
+    registered_nodes = {}
+    for node in entry_points():
+        if node.group.startswith(_MODULE_NAME):
+            module = tuple(node.group.split(".") + [node.name])
+            if module in registered_nodes:
+                warn(
+                    f"Name clash! Will overwrite {module} with currently registered "
+                    f"node {registered_nodes[module]} with new node {node}!"
+                )
+            registered_nodes[module] = node
+
+    return registered_nodes
+
+
+def _gen_name(module_path_list):
+    if len(module_path_list) == 2:
+        if module_path_list[0] != _MODULE_NAME:
+            raise ValueError(f"Unintended use, module path must start with {_MODULE_NAME},"
+                             f"but got {module_path_list}.")
+        return _DEFAULT_SUB_MODULE_NAME, module_path_list[1]
+    else:
+        return ".".join(module_path_list[1:-1]), module_path_list[-1]
+
+
+def _gen_sub_module(module_path_list):
+    _full_name = ".".join(module_path_list)
+    _parent_mod_path = ".".join(module_path_list[:-1])
+    _new_mod_name = module_path_list[-1]
+    if _parent_mod_path not in _SUB_MODULES and _parent_mod_path != "":
+        _gen_sub_module(module_path_list[:-1])
+
+    _mod = types.ModuleType(_new_mod_name)
+    if _parent_mod_path == "":
+        globals()[_new_mod_name] = _mod
+    else:
+        setattr(_SUB_MODULES[_parent_mod_path], _new_mod_name, _mod)
+    _SUB_MODULES[_full_name] = _mod
+
+
+for _module_path_list, _entry_point in _find_entry_points().items():
+    _mod_path, _mod_name = _gen_name(_module_path_list)
+    if _mod_path not in _SUB_MODULES:
+        _gen_sub_module(_module_path_list[1:-1])
+    setattr(_SUB_MODULES[_mod_path], _mod_name, _entry_point.load())
