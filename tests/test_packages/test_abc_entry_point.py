2b9bf1c61df7cc249f2d67a5ef52aec14279a1c6
Niklas Siemer <70580458+niklassiemer@users.noreply.github.com>
2025-05-19 15:24:56 +0200

    Add a + b + c test

---
+++ tests/test_packages/test_abc_entry_point.py
@@ -0,0 +1,26 @@
+import unittest
+
+import pyiron_node_store
+
+
+class MyTestCase(unittest.TestCase):
+    def test_entry_point_keys(self):
+        self.assertEqual(
+            {
+                key: [node for node in dir(value) if not node.startswith("_")]
+                for key, value in pyiron_node_store._SUB_MODULES.items()
+            },
+            {
+                "nodes": ["math", "prod", "sum", "mathematics"],
+                "mathematics": ["prod", "sum", "sum_b"],
+                "math": ["sum", "prod"],
+                "some": ["long"],
+                "some.long": ["package"],
+                "some.long.package": ["path"],
+                "some.long.package.path": ["sum"],
+            },
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
