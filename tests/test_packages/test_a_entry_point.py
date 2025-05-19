090a383255b83f98261b57783ae5548144ca8823
Niklas Siemer <70580458+niklassiemer@users.noreply.github.com>
2025-05-19 13:57:06 +0200

    only test for keys

--- tests/test_packages/test_a_entry_point.py
+++ tests/test_packages/test_a_entry_point.py
@@ -3,8 +3,8 @@ import pyiron_node_store
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(pyiron_node_store.get_pyiron_nodes_dict(), {"nodes": {}})
+    def test_entry_point_keys(self):
+        self.assertEqual([n for n in pyiron_node_store.get_pyiron_nodes_dict()], ["nodes"])
 
 
 if __name__ == '__main__':
